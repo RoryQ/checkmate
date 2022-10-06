7dfde6d9b7fbf9094ec4bad014429ae74e4a26dd
Rory Quinn <quinn.rory@gmail.com>
2022-10-06 21:05:19 +1100

    fail if any are incomplete

--- checkmate/checkmate.go
+++ checkmate/checkmate.go
@@ -2,6 +2,7 @@ package checkmate
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"github.com/sethvargo/go-githubactions"
@@ -17,14 +18,21 @@ func Run(ctx context.Context, cfg *Config, action *githubactions.Action) error {
 
 	checklists := Parse(descriptionPR)
 
-	action.Infof("Checklists: %v", checklists)
+	action.Debugf("Checklists: %v", checklists)
 
+	allChecked := true
 	for _, checklist := range checklists {
+		allChecked = allChecked && checklist.AllChecked()
+
 		if !checklist.AllChecked() {
 			headerNoPrefix := strings.TrimPrefix(strings.TrimSpace(checklist.Header), "#")
 			action.Errorf("Checklist not completed %s", headerNoPrefix)
 		}
 		action.AddStepSummary(checklist.Summary())
+	}
+
+	if !allChecked {
+		return errors.New("not all checklists are completed")
 	}
 
 	return nil
